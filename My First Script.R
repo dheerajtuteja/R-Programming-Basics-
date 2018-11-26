993dc2f070e370eda7b33240c3bc52f61b650ff6
dheerajtuteja <44654396+dheerajtuteja@users.noreply.github.com>
2018-11-26 13:25:09 +0100

    Add files via upload
    Baby Steps towards learning R Programming.
    Will keep on adding new files in future.

---
+++ My First Script.R
@@ -0,0 +1,28 @@
+#First Script
+"Hello World!"
+
+#Integer
+
+y<-20L
+typeof(y)
+
+#Double
+
+z<-5.433
+typeof(z)
+
+#Complex
+x<-5+3i
+typeof(x)
+
+#Character
+A<-"Sunny"
+typeof(A)
+
+#Logical
+
+Q1<-T
+typeof(Q1)
+
+Q2<-F
+typeof(Q2)
---
+++ Using Variables.R
@@ -0,0 +1,30 @@
+#Declaring Variables
+a <- 5
+b <- 10
+#Addition Operation
+c <- a+b
+#Print Variable
+c
+
+#Declaring Variables
+Var1 <- 2.5
+Var2 <- 4
+#Multiplication Operation
+Result <-Var1 * Var2
+#Print Variable
+Result
+
+
+#Variables with Functions
+Square_Root<-sqrt(Var2)
+#Print Variable
+Square_Root
+
+
+#Character Variables
+greeting<-"Hello"
+name<-"Kiki"
+#Concatenate Character Variables
+message<-paste(greeting,name)
+#Print Variable
+message
