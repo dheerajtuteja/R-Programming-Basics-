821a3154ad54e16865675d457d971d0249db61d3
dheerajtuteja <44654396+dheerajtuteja@users.noreply.github.com>
2018-11-26 14:31:43 +0100

    Add files via upload
    Use of Logical Operators in R Programming (Basic Understanding)

---
+++ Logical Operators.R
@@ -0,0 +1,34 @@
+#Logical
+#TRUE T
+#FALSE F
+
+# ==
+# != (Not Equal to)
+# <
+# >
+# <=
+# >=
+# ! (Not)
+# | (Single Pipe is called OR in R Programming)
+# & AND
+# isTrue(X)
+
+
+# >
+Result <- 10>6
+typeof(Result)
+Result
+
+# ! (Not)
+Result1 <- !(10>6)
+Result1
+
+# | (Single Pipe is called OR in R Programming)
+
+Result | Result1
+
+# & AND
+Result & Result1
+
+# isTrue(X)
+isTRUE(Result1)
